886e53c5c87edd012fc4103638979a3144064bf6
Naveen24SK <sknaveenvj@gmail.com>
2024-03-20 09:51:21 +0530

    Head

--- src/components/HomePage/Header.jsx
+++ src/components/HomePage/Header.jsx
@@ -1,60 +1,10 @@
 import React from 'react';
 import './Header.css';
-import { FaRegUserCircle } from "react-icons/fa";
+import { FaRegUserCircle, FaSearch } from "react-icons/fa"; // Import FaSearch
 import VerticalNav from '../../components/verticalNavbar/verticalNav.jsx';
 import logo from '../../Assets/best_logo.png';
 import Swal from 'sweetalert2';
 import { useNavigate } from 'react-router-dom';
-import { styled, alpha } from '@mui/material/styles';
-import InputBase from '@mui/material/InputBase';
-// import SearchIcon from '@mui/icons-material/Search';
-
-// Define styled components for the search bar
-const Search = styled('div')(({ theme }) => ({
- position: 'relative',
- borderRadius: theme.shape.borderRadius, // Apply border radius
- backgroundColor: alpha(theme.palette.common.white, 0.75),
- border:'#00000',
-
- '&:hover': {
-    backgroundColor: alpha(theme.palette.common.black, 0.25),
-    border:'#000000'
- },
- marginLeft: 0,
- width: '100%',
- [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(2),
-    width: 'auto',
- },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
- padding: theme.spacing(0, 2),
- height: '100%',
- position: 'absolute',
- pointerEvents: 'none',
- display: 'flex',
- alignItems: 'center',
- justifyContent: 'center',
- borderRadius: theme.shape.borderRadius, // Apply border radius to the icon wrapper
- backgroundColor: alpha(theme.palette.common.white, 0.15), // Add a background color to the icon wrapper
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
- color: 'inherit',
- width: '100%',
- '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
- },
-}));
 
 const Header = () => {
  const navigate = useNavigate();
@@ -115,16 +65,11 @@ const Header = () => {
           </ul>
         </nav>
         <div className="user-actions">
-          <input className='search-box' type='text' placeholder='Search'  />
-          {/* <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search> */}
+          {/* Wrap the input and the icon in a div */}
+          <div className="search-container">
+            <input className='search-box' type='text' placeholder='Search' />
+            <FaSearch className="search-icon" /> {/* Add the search icon */}
+          </div>
           <a className='profile-icon' href="#userLogin" onClick={showLoginModal}><FaRegUserCircle /></a>
         </div>
       </div>
